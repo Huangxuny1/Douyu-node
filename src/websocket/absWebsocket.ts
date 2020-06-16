7d261e5de27c700d63425f2f39803ec6a7e49cf6
Huangxuny1 <ghuangxunyi@gmail.com>
2020-06-17 00:13:01 +0800

    douyu websocket

--- src/websocket/absWebsocket.ts
+++ src/websocket/absWebsocket.ts
@@ -1,1 +1,22 @@
-import ws from 'ws';
+import * as Websocket from 'ws';
+import BufferCoder from "../serializer/BufferCoder"
+import { log4js } from "../global"
+
+interface IDouyuWebsocket {
+    ws: WebSocket;
+    heartbeat: any;
+}
+
+abstract class AbsDouyuWebsocket {
+    private logger = log4js.getLogger('AbsDouyuWebsocket');
+
+
+    public constructor() {
+        // todo constuctor
+    }
+
+    public addRoom = async (roomid: string | number): Promise<void> => {
+        const dyws = new Websocket('wss://danmuproxy.douyu.com:8506/');
+        
+    }
+}
