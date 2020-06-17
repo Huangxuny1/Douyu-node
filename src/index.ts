f1e42a785fc6d0614817df922863537636faacf3
Huangxuny1 <ghuangxunyi@gmail.com>
2020-06-17 21:30:26 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { log4js, douyu } from './global';
-import Barrage from './websocket/barrageFetcher'
-import ProxyWorker from './websocket/proxyWorker'
+import DouyuClient from './websocket/DouyuClient'
 
 
 
@@ -11,42 +10,18 @@ const logger = log4js.getLogger('index');
 (
     async () => {
         // logger.info(await douyu.login());
-        // logger.debug(await douyu.whoami());
-        let cookies = await douyu.login();
-        logger.info(" login status : ", douyu.alreadyLogined)
-        let proxyWSS = await douyu.fetchProxy(9999)
-        let server: any = proxyWSS[Math.floor(Math.random() * proxyWSS.length)]
-        let worker = new ProxyWorker("wss://" + server.domain + ":" + server.port, 9999, cookies)
+        let client = new DouyuClient(73966)
+        await client.start()
 
-        await worker.connect()
-        await worker.onopen((obj) => {
-            logger.warn(obj)
-        })
+        logger.debug(await douyu.whoami());
 
-        let barrage = new Barrage('wss://danmuproxy.douyu.com:8506/', 9999);
-        await barrage.connect()
-        await barrage.onopen((obj) => {
-            if (obj.type == 'chatmsg') {
-                logger.info('[%s] - %s(lv:%s)[%s:%s]:\t%s', obj.rid, obj.nn, obj.level, obj.bnn, obj.bl, obj.txt);
-            }
-        });
+        let worker = client.getWorker;
 
         let input = process.openStdin()
         input.addListener("data", function (d) {
-            // note:  d is an object, and when converted to a string it will
-            // end with a linefeed.  so we (rather crudely) account for that  
-            // with toString() and then substring() 
             logger.error("you entered: [" + d.toString().trim() + "]");
             worker.sendBarrage(d.toString().trim())
+
         });
-
-        // let a = new Barrage('wss://danmuproxy.douyu.com:8506/', 9999);
-        // await a.connect()
-        // await a.onopen((obj) => {
-        //     if (obj.type == 'chatmsg') {
-        //         logger.info('[%s] - %s(lv:%s)[%s:%s]:\t%s', obj.rid, obj.nn, obj.level, obj.bnn, obj.bl, obj.txt);
-        //     }
-        // });
-
     }
 )()
---
+++ src/websocket/DouyuClient.ts
@@ -0,0 +1,44 @@
+import { log4js, douyu } from '../global'
+import ProxyWorker from './proxyWorker'
+import Barrage from './barrageFetcher'
+
+const logger = log4js.getLogger("DouyuClient")
+export default class DouyuClient {
+    private roomid: string | number;
+    private worker!: ProxyWorker;
+    private barrage!: Barrage;
+
+    public constructor(roomid: string | number) {
+        this.roomid = roomid;
+    }
+
+    public start = async (): Promise<DouyuClient> => {
+        let cookies = await douyu.login();
+        logger.info(" login status : ", douyu.alreadyLogined)
+
+        let proxyWSS = await douyu.fetchProxy(this.roomid)
+        let server: any = proxyWSS[Math.floor(Math.random() * proxyWSS.length)]
+        this.worker = new ProxyWorker("wss://" + server.domain + ":" + server.port, this.roomid, cookies)
+        await this.worker.connect()
+        await this.worker.onopen((obj) => {
+            logger.warn(obj)
+        })
+
+        this.barrage = new Barrage('wss://danmuproxy.douyu.com:8506/', this.roomid);
+        await this.barrage.connect()
+        await this.barrage.onopen((obj) => {
+            if (obj.type == 'chatmsg') {
+                logger.info('[%s] - %s(lv:%s)[%s:%s]:\t%s', obj.rid, obj.nn, obj.level, obj.bnn, obj.bl, obj.txt);
+            }
+        });
+        return this
+    }
+
+    public get getWorker(): ProxyWorker {
+        return this.worker;
+    }
+
+    public get getBarrageFetcher(): Barrage {
+        return this.barrage;
+    }
+}
